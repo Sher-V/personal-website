a7a74c4b2176380b4bb67cdda49b79d261e27f27
Sheremetov Vadim <beingalwaysrightthere@gmail.com>
2021-08-22 13:40:06 +0300

    list disc inside

--- src/components/modules/work-experience-card/work-experience-card.tsx
+++ src/components/modules/work-experience-card/work-experience-card.tsx
@@ -15,7 +15,7 @@ const WorkExperienceCard: React.FC<Props> = ({
     <div className={"text-black font-medium text-secondary"}>{position}</div>
     <div className={"text-black font-medium text-md mt-10"}>{company}</div>
     <div className={"text-secondary font-secondary"}>
-      <ul className={"list-disc"}>
+      <ul className={"list-disc list-inside"}>
         {description.map((point, i) => (
           <li key={i}>{point}</li>
         ))}
